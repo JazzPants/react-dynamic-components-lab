1a838b771dd8c1d80d5f212be4c2e229675d8de7
JazzPants <leighton.l14@gmail.com>
2022-01-02 15:58:48 +1100

    Done!

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,7 @@
 //your code here
 import React, {Component} from 'react';
 
+//Comment is inside BlogPost render, so it takes any properties, i.e. the comments array, because commentText=comments[]
 class Comment extends Component {
     render() {
       return (
